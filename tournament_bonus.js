59a9a6004924becae4c42b20e65ab8cd625a7507
MViktorE <87064729+MViktorE@users.noreply.github.com>
2024-08-26 23:15:58 +0300

    Create tournament_bonus.js

---
+++ tournament_bonus.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    document.querySelectorAll('.ttext.lobby-item__name--bonus').forEach(item => {
+        item.closest('.lobby-item').querySelector('.lobby-item__green')?.click();
+    document.getElementById('react-confirm-alert')?.querySelector('.green').click();
+    })
+}, 100)
